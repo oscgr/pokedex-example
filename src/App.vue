b6e46b94c9ef3e1b34bc0f9abe1a417ac87c4c36
ogr <ogr@ehtrace.com>
2023-09-12 14:00:07 +0200

    infinite list

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,7 @@
       </v-app-bar>
 
       <v-navigation-drawer v-model="opened" :rail="rail" @click="rail = false" rail-width="90">
-        <v-list density="comfortable" v-model:selected="listSelected" mandatory>
-          <v-list-subheader>Liste des Pokémon</v-list-subheader>
-          <ListItemPokemon v-for="pokemon in results" :key="pokemon.name" :name="pokemon.name" />
-        </v-list>
+        <ListPokemon />
       </v-navigation-drawer>
 
       <v-main>
@@ -26,22 +23,11 @@
 
 <script setup lang="ts">
 import useDrawerStore from '@/stores/drawer'
-import useQuery from '@/stores/query'
-import { onMounted, ref, watch } from 'vue'
-import { NamedAPIResource } from '@/types/api/utility'
+import { watch } from 'vue'
 import PokemonCard from '@/components/PokemonCard.vue'
-import ListItemPokemon from '@/ListItemPokemon.vue'
-import usePokemonSelection from '@/stores/pokemonSelection'
+import ListPokemon from '@/components/ListPokemon/ListPokemon.vue'
 
 const { rail, opened, toggle, watcher, mobile } = useDrawerStore()
-
-const results = ref<NamedAPIResource[]>([])
-const { get } = useQuery('pokemon')
-const { listSelected } = usePokemonSelection()
-
-onMounted(async () => {
-  results.value = (await get(0)) || []
-})
 
 watch(mobile, watcher)
 </script>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -8,9 +8,11 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles' // Composables
 import { createVuetify } from 'vuetify' // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+import { VInfiniteScroll } from 'vuetify/labs/VInfiniteScroll'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  components: { VInfiniteScroll },
   theme: {
     defaultTheme: 'light',
     themes: {
